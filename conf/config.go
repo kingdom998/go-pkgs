7814ad5fc454029c433e5fe8128dacb404c15df8
kunpeng <kupeng@zy.com>
2024-09-11 11:28:11 +0800

    update worker comment

--- conf/config.go
+++ conf/config.go
@@ -17,7 +17,7 @@ type Config struct {
 	Storage bos.Config      `json:"storage"`
 	WebUI   http.Config     `json:"webUI"`
 	ComfyUI http.Config     `json:"comfyUI"`
-	Worker  http.Config     `json:"openai"`
+	Worker  http.Config     `json:"worker"`
 }
 
 func New(confFile string) *Config {
